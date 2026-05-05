494e02d0e122674e70cb0d02c6c60a04005be27c
Sai Kham Hein <saikhamhein@gmail.com>
2026-05-05 16:04:40 +0800

    add overseer task runner

---
+++ nvim/dot-config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/overseer.nvim",
+  ---@module 'overseer'
+  ---@type overseer.SetupOpts
+  opts = {},
+  keys = {
+    { "<leader>t", "<cmd>OverseerRun<cr>", desc = "Run task" },
+  },
+}
